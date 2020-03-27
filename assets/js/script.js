9655f13e6ed035012ef602223aefe9e21d2f8b96
Java152001 <java152001@gmail.com>
2020-03-27 15:38:52 -0700

    Added starting styles and example card

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+let imageDirectory = "assets/images/"
+let participants = [
+    {
+        name : "Franz Ferdinand",
+        image : imageDirectory + "Franz_Ferdinand.jpg"
+    },
+    {
+        name : "Czar Nicholas",
+        image : imageDirectory + "Czar_Nicholas.jpg"
+    },
+    {
+        name : "David Lloyd George",
+        image : imageDirectory + "David_Lloyd_George.jpg"
+    },
+    {
+        name : "Franz Joseph",
+        image : imageDirectory + "Franz-Joseph.jpg"
+    },
+    {
+        name : "King George V",
+        image : imageDirectory + "king_george-v.jpg"
+    },
+    {
+        name : "Raymond Poincaré",
+        image : imageDirectory + "Raymond_Poincaré.jpg"
+    }, 
+    {
+        name : "Serbia",
+        image : imageDirectory + "serbia.jpg"
+    },
+    {
+        name : "Sultan Mehmed V",
+        image : imageDirectory + "sultan_mehmed_V.jpg"
+    },
+    {
+        name : "The Black Hand",
+        image : imageDirectory + "The-Black-Hand.jpg"
+    },
+    {
+        name : "Wilhelm II",
+        image : imageDirectory + "wilhelm-ii.jpg"
+    }
+]
